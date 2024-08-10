5c32fc57f4b0e76e37fae61e2f48532b8b5a385a
Amantha Tharusha <pmatsrikantha@gmail.com>
2024-08-10 10:28:25 +0530

    Practical 8 done

---
+++ practical8/q1.scala
@@ -0,0 +1,37 @@
+object q1 {
+  def encrypt(text: String, shift: Int): String = {
+    text.map { char =>
+      if (char.isLetter) {
+        val base = if (char.isLower) 'a' else 'A'
+        ((char - base + shift) % 26 + base).toChar
+      } else {
+        char
+      }
+    }
+  }
+
+  def decrypt(text: String, shift: Int): String = {
+    encrypt(text, 26 - shift)
+  }
+
+  def cipher(
+      process: (String, Int) => String,
+      text: String,
+      shift: Int
+  ): String = {
+    process(text, shift)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val plaintext = "Hello, world!"
+    val shift = 1
+
+    println(s"Plain text: $plaintext")
+
+    val encryptedtext = cipher(encrypt, plaintext, shift)
+    println(s"Encrypted Text: $encryptedtext")
+
+    val decryptedtext = cipher(decrypt, encryptedtext, shift)
+    println(s"Decrypted Text: $decryptedtext")
+  }
+}
